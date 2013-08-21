9e54927eb5f2481ee1ff037ed58c9515af90e37c
Kyle Millns <kyle.millns@gmail.com>
2013-08-21 15:24:58 -0500

    Day 2 through step 16

--- js/app.js
+++ js/app.js
@@ -96,8 +96,53 @@
 	});
 })();
 
+// components/AudioPlayer.js
 (function (App) {
 	App.AudioPlayerComponent = Ember.Component.extend({
-		classNames: ['audio-control']
+		classNames: ['audio-control'],
+		currentTime: 0,
+		duration: null,
+		isLoaded: false,
+		isPlaying: false,
+		play: function () {
+			this.$('audio')[0].play();
+		},
+		pause: function () {
+			this.$('audio')[0].pause();
+		},
+		didInsertElement: function () {
+			var component = this;
+
+			component.$('audio')
+				.on('timeupdate', function () {
+					component.set('currentTime', Math.floor(this.currentTime));
+				})
+				.on('loadeddata', function () {
+					component.set('duration', Math.floor(this.duration));
+					component.set('isLoaded', true);
+				})
+				.on('play', function () {
+					component.set('isPlaying', true);
+				})
+				.on('pause', function () {
+					component.set('isPlaying', false);
+				});
+		}
+	});
+})(window.App);
+
+// components/ToggleTime.js
+(function (App) {
+	App.ToggleTimeComponent = Ember.Component.extend({
+		showingCurrent: true,
+		toggleTime: function () {
+			this.set('showingCurrent', !this.get('showingCurrent'));
+		},
+		remainingTime: function () {
+			var currentTime = this.get('currentTime');
+			var totalTime = this.get('duration');
+
+			return totalTime - currentTime;
+		}.property('currentTime', 'duration')
 	});
 })(window.App);
